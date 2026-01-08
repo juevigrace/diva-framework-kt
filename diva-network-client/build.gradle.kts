bcc849d476eed0146ef97325e14bb6608b3d2629
Daniel Zabala <juev1grace@gmail.com>
2026-01-08 15:56:57 -0400

    fix ktor json dependency

--- diva-network-client/build.gradle.kts
+++ diva-network-client/build.gradle.kts
@@ -10,6 +10,7 @@ kotlin {
             api(libs.ktor.client.core)
             api(libs.ktor.client.content.negotiation)
             api(libs.ktor.client.logging)
+            api(libs.ktor.serialization.kotlinx.json)
         }
 
         androidMain.dependencies {
