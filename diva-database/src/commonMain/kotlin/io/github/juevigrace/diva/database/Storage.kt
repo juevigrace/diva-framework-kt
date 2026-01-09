179d0b901b8d4b9d8e4c89e0d551d1a8c13272a6
Daniel Zabala <juev1grace@gmail.com>
2026-01-09 15:50:54 -0400

    fix database errors

--- diva-database/src/commonMain/kotlin/io/github/juevigrace/diva/database/Storage.kt
+++ diva-database/src/commonMain/kotlin/io/github/juevigrace/diva/database/Storage.kt
@@ -1,29 +1,29 @@
 package io.github.juevigrace.diva.database
 
-import io.github.juevigrace.diva.core.errors.DivaError
 import io.github.juevigrace.diva.core.DivaResult
 import io.github.juevigrace.diva.core.Option
+import io.github.juevigrace.diva.core.errors.DivaError
 import kotlinx.coroutines.flow.Flow
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
 interface Storage<T : Any> {
-    suspend fun count(): DivaResult<Long, DivaError>
+    suspend fun count(): DivaResult<Long, DivaError.DatabaseError>
 
-    suspend fun getAll(limit: Int = 0, offset: Int = 0): DivaResult<List<T>, DivaError>
+    suspend fun getAll(limit: Int = 0, offset: Int = 0): DivaResult<List<T>, DivaError.DatabaseError>
 
-    suspend fun getAllFlow(limit: Int = 0, offset: Int = 0): Flow<DivaResult<List<T>, DivaError>>
-
-    @OptIn(ExperimentalUuidApi::class)
-    suspend fun getById(id: Uuid): DivaResult<Option<T>, DivaError>
+    suspend fun getAllFlow(limit: Int = 0, offset: Int = 0): Flow<DivaResult<List<T>, DivaError.DatabaseError>>
 
     @OptIn(ExperimentalUuidApi::class)
-    suspend fun getByIdFlow(id: Uuid): Flow<DivaResult<Option<T>, DivaError>>
-
-    suspend fun insert(item: T): DivaResult<Unit, DivaError>
-
-    suspend fun update(item: T): DivaResult<Unit, DivaError>
+    suspend fun getById(id: Uuid): DivaResult<Option<T>, DivaError.DatabaseError>
 
     @OptIn(ExperimentalUuidApi::class)
-    suspend fun delete(id: Uuid): DivaResult<Unit, DivaError>
+    suspend fun getByIdFlow(id: Uuid): Flow<DivaResult<Option<T>, DivaError.DatabaseError>>
+
+    suspend fun insert(item: T): DivaResult<Unit, DivaError.DatabaseError>
+
+    suspend fun update(item: T): DivaResult<Unit, DivaError.DatabaseError>
+
+    @OptIn(ExperimentalUuidApi::class)
+    suspend fun delete(id: Uuid): DivaResult<Unit, DivaError.DatabaseError>
 }
