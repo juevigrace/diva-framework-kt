e83a1e8ebea51ce9a0034e38d040969a371f6d40
JueViGrace <juev1grace@gmail.com>
2026-03-22 17:38:35 -0400

    add nav display options

--- diva-ui/src/commonMain/kotlin/io/github/juevigrace/diva/ui/components/navigation/Navigator.kt
+++ diva-ui/src/commonMain/kotlin/io/github/juevigrace/diva/ui/components/navigation/Navigator.kt
@@ -1,15 +1,26 @@
 package io.github.juevigrace.diva.ui.components.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.SizeTransform
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.NavEntryDecorator
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
+import androidx.navigation3.scene.Scene
+import androidx.navigation3.scene.SceneStrategy
+import androidx.navigation3.scene.SinglePaneSceneStrategy
 import androidx.navigation3.ui.NavDisplay
+import androidx.navigation3.ui.defaultPopTransitionSpec
+import androidx.navigation3.ui.defaultPredictivePopTransitionSpec
+import androidx.navigation3.ui.defaultTransitionSpec
+import androidx.navigationevent.NavigationEvent
 import io.github.juevigrace.diva.ui.navigation.BackStack
 import io.github.juevigrace.diva.ui.navigation.Navigator
 
@@ -17,15 +28,33 @@ import io.github.juevigrace.diva.ui.navigation.Navigator
 fun <T : NavKey> Navigator(
     startDestination: T,
     modifier: Modifier = Modifier,
+    contentAlignment: Alignment = Alignment.TopStart,
     entryDecorators: List<NavEntryDecorator<T>> = listOf(rememberSaveableStateHolderNavEntryDecorator()),
+    sceneStrategy: SceneStrategy<T> = SinglePaneSceneStrategy(),
+    sizeTransform: SizeTransform? = null,
+    transitionSpec: AnimatedContentTransitionScope<Scene<T>>.() -> ContentTransform =
+        defaultTransitionSpec(),
+    popTransitionSpec: AnimatedContentTransitionScope<Scene<T>>.() -> ContentTransform =
+        defaultPopTransitionSpec(),
+    predictivePopTransitionSpec:
+    AnimatedContentTransitionScope<Scene<T>>.(
+        @NavigationEvent.SwipeEdge Int
+    ) -> ContentTransform =
+        defaultPredictivePopTransitionSpec(),
     entryProvider: (key: T) -> NavEntry<T>,
 ) {
     val navigator: Navigator<T> = remember { Navigator.newInstance(startDestination) }
     Navigator(
         navigator = navigator,
         modifier = modifier,
+        contentAlignment = contentAlignment,
         onBack = navigator::pop,
         entryDecorators = entryDecorators,
+        sceneStrategy = sceneStrategy,
+        sizeTransform = sizeTransform,
+        transitionSpec = transitionSpec,
+        popTransitionSpec = popTransitionSpec,
+        predictivePopTransitionSpec = predictivePopTransitionSpec,
         entryProvider = entryProvider
     )
 }
@@ -34,16 +63,34 @@ fun <T : NavKey> Navigator(
 fun<T : NavKey> Navigator(
     navigator: Navigator<T>,
     modifier: Modifier = Modifier,
+    contentAlignment: Alignment = Alignment.TopStart,
     onBack: () -> Unit = { navigator.pop() },
     entryDecorators: List<NavEntryDecorator<T>> = listOf(rememberSaveableStateHolderNavEntryDecorator()),
+    sceneStrategy: SceneStrategy<T> = SinglePaneSceneStrategy(),
+    sizeTransform: SizeTransform? = null,
+    transitionSpec: AnimatedContentTransitionScope<Scene<T>>.() -> ContentTransform =
+        defaultTransitionSpec(),
+    popTransitionSpec: AnimatedContentTransitionScope<Scene<T>>.() -> ContentTransform =
+        defaultPopTransitionSpec(),
+    predictivePopTransitionSpec:
+    AnimatedContentTransitionScope<Scene<T>>.(
+        @NavigationEvent.SwipeEdge Int
+    ) -> ContentTransform =
+        defaultPredictivePopTransitionSpec(),
     entryProvider: (key: T) -> NavEntry<T>
 ) {
     val backStack: BackStack<T> by navigator.backStack.collectAsStateWithLifecycle()
     NavDisplay(
         modifier = modifier,
         backStack = backStack.entries,
+        contentAlignment = contentAlignment,
         onBack = onBack,
         entryDecorators = entryDecorators,
+        sceneStrategy = sceneStrategy,
+        sizeTransform = sizeTransform,
+        transitionSpec = transitionSpec,
+        popTransitionSpec = popTransitionSpec,
+        predictivePopTransitionSpec = predictivePopTransitionSpec,
         entryProvider = entryProvider
     )
 }
